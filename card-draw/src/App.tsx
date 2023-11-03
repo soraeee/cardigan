6a0f7c75353ce38d7d37dd2f34608282c1c3fd57
sorae <46469913+soraeee@users.noreply.github.com>
2023-11-03 04:13:09 -0400

    CSS styling

--- card-draw/src/App.tsx
+++ card-draw/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import './App.css'
+import './index.css'
+import './reset.css'
 
 import CardDraw from './CardDraw'
 
--- card-draw/src/CardDraw.tsx
+++ card-draw/src/CardDraw.tsx
@@ -1,17 +1,20 @@
 import chartJSON from './assets/rip135-assets/pack.json'
 import { useState } from 'react';
-import './App.css'
 import './index.css'
+import './reset.css'
 
 function CardDraw() {
 
 	interface Chart {
 		id: number;
 		title: string;
+		subtitle: string;
 		artist: string;
-		difficulty: Number;
-		displaybpm: String[]; // fuck off
-		tier: Number;
+		difficulty: number;
+		difficultyslot: string;
+		displaybpm: string; // fuck off
+		tier: number;
+		hasBanner: boolean;
 	}
 
 	const chartarr: Chart[] = [];
@@ -24,19 +27,33 @@ function CardDraw() {
 	chartJSON.charts.forEach(chart => {
 		{
 			// Convert "[Txx] name" format into a Tier value
-			let tier: Number = Number(chart.title.substring(2, 4))
+			let tier: number = Number(chart.title.substring(2, 4))
 			// Use transliterated titles if available
-			let songtitle: string = (chart.titletranslit != "") ? chart.titletranslit : chart.title
+			let songtitle: string = (chart.titletranslit != "") ? chart.titletranslit.substring(6, chart.titletranslit.length) : chart.title.substring(6, chart.title.length)
 			let songartist: string = (chart.artisttranslit != "") ? chart.artisttranslit : chart.artist
+			let songsubtitle: string = (chart.subtitletranslit != "") ? chart.subtitletranslit : chart.subtitle
+
+			let displaybpmFormatted = ""
+			if (chart.displaybpm[0] === chart.displaybpm[1]) {
+				displaybpmFormatted = chart.displaybpm[0]
+			} else {
+				displaybpmFormatted = chart.displaybpm[0] + " - " + chart.displaybpm[1]
+			}
+
+			let hasBanner = false
+			if (chart.bnpath != "") hasBanner = true
 
 			// Add properly formatted metadata to array
 			chartarr.push({
 				id: Number(chart.sid),
 				title: songtitle,
 				artist: songartist,
+				subtitle: songsubtitle,
 				difficulty: Number(chart.difficulties[0].difficulty),
-				displaybpm: chart.displaybpm,
+				difficultyslot: chart.difficulties[0].slot,
+				displaybpm: displaybpmFormatted,
 				tier: tier,
+				hasBanner: hasBanner,
 			})
 		}
 	});
@@ -48,7 +65,6 @@ function CardDraw() {
 		chartIds[i] = i
 	}
 
-
 	function swapIndices(a: number, b: number, array: number[]) {
 		let c = array[a]
 		array[a] = array[b]
@@ -59,7 +75,7 @@ function CardDraw() {
 		return Math.floor(Math.random() * max);
 	}
 
-
+	// Draw n number of charts from the available pool
 	function draw() {
 		// Fisher-Yates shuffle
 		for (let i = chartIds.length - 1; i >= 0; i--) {
@@ -74,16 +90,48 @@ function CardDraw() {
 		setSpread(drawnIds)
 	}
 
+	function truncate(str: string){
+		let n = 40
+		if (str.length >= n) {
+			return str.substring(0, n) + "...";
+		}
+		return str
+	}
+
+
+	// todo: split cards into its own component
+	// todo: make displaybpm display not cursed
 	return (
 		<>
 			<div className="header">
 				<button onClick={draw} className="button">Draw</button>
 			</div>
-			<div className="cards">
+			<div className="cardDisplay">
 				{chartarr.filter((chart) => {
 					if (spread.includes(chart["id"])) return chart
 				}).map((chart) => {
-					return <div key={chart.id}>{chart.title}</div>
+					// Stolen from DDRTools sorry man lol
+					let bannerBackground = {};
+					if (chart.hasBanner) {
+						bannerBackground = {
+							"background": `linear-gradient(45deg, #161616FF, #9EABFF88), url("src/assets/rip135-assets/bn-${chart.id}.png")`,
+							"background-size": "100%",
+							"background-position": "center",
+						};
+					}
+					return <div key={chart.id} className="card" style={bannerBackground}>
+
+						<div className="card-text-upper">
+							<p className="card-text-artist">{chart.artist}</p>
+							<p className="card-text-title">{truncate(chart.title)}</p>
+							<p className="card-text-subtitle">{chart.subtitle}</p>
+						</div>
+						<div className="card-text-lower">
+							<p className="card-text-tier">Tier {chart.tier}</p>
+							<p className="card-text-diff">{chart.difficultyslot} {chart.difficulty}</p>
+							<p className="card-text-bpm">{chart.displaybpm} BPM</p>
+						</div>
+					</div>
 				})}
 			</div>
 		</>
