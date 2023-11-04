0ea282a8d931ee0052cbeeb6d4c393e0b3e9dddb
Spencer Gunning <swcgunning@gmail.com>
2023-11-03 23:19:00 -0400

    CSS, JS improvements

--- card-draw/src/CardDraw.tsx
+++ card-draw/src/CardDraw.tsx
@@ -12,8 +12,10 @@ function CardDraw() {
 		artist: string;
 		difficulty: number;
 		difficultyslot: string;
-		displaybpm: string; // fuck off
+		displaybpm: number[];
+		bpmstring: string; // hi sore
 		tier: number;
+		nocmod: boolean;
 		hasBanner: boolean;
 	}
 
@@ -27,17 +29,18 @@ function CardDraw() {
 	chartJSON.charts.forEach(chart => {
 		{
 			// Convert "[Txx] name" format into a Tier value
-			let tier: number = Number(chart.title.substring(2, 4))
+			const tier: number = Number(chart.title.substring(2, 4))
 			// Use transliterated titles if available
-			let songtitle: string = (chart.titletranslit != "") ? chart.titletranslit.substring(6, chart.titletranslit.length) : chart.title.substring(6, chart.title.length)
-			let songartist: string = (chart.artisttranslit != "") ? chart.artisttranslit : chart.artist
-			let songsubtitle: string = (chart.subtitletranslit != "") ? chart.subtitletranslit : chart.subtitle
+			const songtitle: string = (chart.titletranslit != "") ? chart.titletranslit.substring(6, chart.titletranslit.length) : chart.title.substring(6, chart.title.length)
+			const songartist: string = (chart.artisttranslit != "") ? chart.artisttranslit : chart.artist
+			const songsubtitle: string = (chart.subtitletranslit != "") ? chart.subtitletranslit : chart.subtitle
 
+			const displaybpm = chart.displaybpm.map(x => Math.round(Number(x)))
 			let displaybpmFormatted = ""
-			if (chart.displaybpm[0] === chart.displaybpm[1]) {
-				displaybpmFormatted = chart.displaybpm[0]
+			if (displaybpm[0] === displaybpm[1]) {
+				displaybpmFormatted = String(Math.round(displaybpm[0]))
 			} else {
-				displaybpmFormatted = chart.displaybpm[0] + " - " + chart.displaybpm[1]
+				displaybpmFormatted = displaybpm[0] + " - " + displaybpm[1]
 			}
 
 			let hasBanner = false
@@ -46,13 +49,16 @@ function CardDraw() {
 			// Add properly formatted metadata to array
 			chartarr.push({
 				id: Number(chart.sid),
-				title: songtitle,
+				title: songtitle.replace(/\(No CMOD\)/,"").trim(),
 				artist: songartist,
 				subtitle: songsubtitle,
 				difficulty: Number(chart.difficulties[0].difficulty),
 				difficultyslot: chart.difficulties[0].slot,
-				displaybpm: displaybpmFormatted,
+				//probably make pack.json have this be Number[] in the json
+				displaybpm: chart.displaybpm.map(x => Number(x)),
+				bpmstring: displaybpmFormatted,
 				tier: tier,
+				nocmod: /\(No CMOD\)/.test(songtitle),
 				hasBanner: hasBanner,
 			})
 		}
@@ -60,13 +66,13 @@ function CardDraw() {
 
 	// Chart IDs remaining in the pool
 	// Todo: reduce this to selected tiers
-	let chartIds: number[] = []
+	const chartIds: number[] = []
 	for (let i = 0; i < chartarr.length; i++) {
 		chartIds[i] = i
 	}
 
 	function swapIndices(a: number, b: number, array: number[]) {
-		let c = array[a]
+		const c = array[a]
 		array[a] = array[b]
 		array[b] = c
 	}
@@ -82,7 +88,7 @@ function CardDraw() {
 			swapIndices(i, getRandomInt(i), chartIds);
 		}
 
-		let drawnIds: number[] = []
+		const drawnIds: number[] = []
 		for (let i = 0; i < numToDraw; i++) {
 			drawnIds[i] = chartIds[i]
 			// todo: remove the id from chartIds once finished. probably need to replace chartIds[i] with chartIds[0] when doing that
@@ -90,17 +96,7 @@ function CardDraw() {
 		setSpread(drawnIds)
 	}
 
-	function truncate(str: string){
-		let n = 40
-		if (str.length >= n) {
-			return str.substring(0, n) + "...";
-		}
-		return str
-	}
-
-
 	// todo: split cards into its own component
-	// todo: make displaybpm display not cursed
 	return (
 		<>
 			<div className="header">
@@ -114,22 +110,40 @@ function CardDraw() {
 					let bannerBackground = {};
 					if (chart.hasBanner) {
 						bannerBackground = {
-							"background": `linear-gradient(45deg, #161616FF, #9EABFF88), url("src/assets/rip135-assets/bn-${chart.id}.png")`,
-							"background-size": "100%",
-							"background-position": "center",
+							"background": `linear-gradient(90deg, #161616DD 20%, #161616AA 100%), url("src/assets/rip135-assets/bn-${chart.id}.png")`,
+							"background-size": "cover",
+							"background-repeat": "no-repeat",
+							"background-position": "100% 50%"
 						};
 					}
+					// difficulty stuff
+					let diffClasses = 'card-diff';
+					switch (chart.difficultyslot) {
+						case 'Novice':		diffClasses += ' diff-n'; break;
+						case 'Easy':		diffClasses += ' diff-e'; break;
+						case 'Medium':		diffClasses += ' diff-m'; break;
+						case 'Hard':		diffClasses += ' diff-h'; break;
+						case 'Challenge':	diffClasses += ' diff-x'; break;
+					}
+					// cmod?
+					let noCmodTag;
+					if (chart.nocmod) {
+						noCmodTag = <div className="card-text-nocmod">No CMOD</div>
+					}
 					return <div key={chart.id} className="card" style={bannerBackground}>
-
-						<div className="card-text-upper">
-							<p className="card-text-artist">{chart.artist}</p>
-							<p className="card-text-title">{truncate(chart.title)}</p>
-							<p className="card-text-subtitle">{chart.subtitle}</p>
+						<div className="card-left">
+							<div className={diffClasses}>
+								<p className='card-text-diff'>{chart.difficulty}</p>
+							</div>
+							<div className="card-meta">
+								<p className="card-text-artist">{chart.artist}</p>
+								<p className="card-text-title">{chart.title}{noCmodTag}</p>
+								<p className="card-text-subtitle">{chart.subtitle}</p>
+							</div>
 						</div>
-						<div className="card-text-lower">
+						<div className="card-right">
 							<p className="card-text-tier">Tier {chart.tier}</p>
-							<p className="card-text-diff">{chart.difficultyslot} {chart.difficulty}</p>
-							<p className="card-text-bpm">{chart.displaybpm} BPM</p>
+							<p className="card-text-bpm">{chart.bpmstring} BPM</p>
 						</div>
 					</div>
 				})}
